0b863fb807674a5246aaba8d4dcbe0d5377b328d
camifernandez2003 <camila.fernandez@ucuenca.edu.ec>
2024-11-19 23:35:15 -0500

    Finalizó el proyecto

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const celdas = [];
-const RETICULA = 4;
-let ancho;
-let alto;
+const RETICULA = 10;
+let ancho; //altura de la celda
+let alto; //anchura de la celda
 
 const azulejos = [];
 const NA = 11; // Número de azulejos
@@ -95,6 +95,9 @@ function preload() {
 function setup() {
   createCanvas(1080, 1080);
 
+  ancho = width / RETICULA;
+  alto = height / RETICULA;
+
   let opcionesI = [];
   for (let i = 0; i < azulejos.length; i++) {
     opcionesI.push(i);
@@ -106,24 +109,101 @@ function setup() {
       opciones: opcionesI,
     };
   }
-  celdas[8].colapsada = true;
-  celdas[3].colapsada = true;
+  //celdas[8].colapsada = true;
+  //celdas[3].colapsada = true;
 
-  celdas[12].colapsada = [5, 6, 8];
-  celdas[4].colapsada = [4, 7, 12];
-  celdas[1].colapsada = [6, 4, 8, 10];
-  celdas[5].colapsada = [11, 6, 4, 8, 10];
+  //celdas[12].opciones = [5, 6, 8];
+  //celdas[4].opciones = [4, 7, 12];
+  //celdas[6].opciones = [9, 7, 12];
+  //celdas[1].opciones = [6, 4, 8, 10];
+  //celdas[5].opciones = [11, 6, 4, 8, 10];
 }
 
 function draw() {
   const celdasDisponibles = celdas.filter((celda) => {
     return celda.colapsada == false;
   });
+
   if (celdasDisponibles.length > 0) {
     celdasDisponibles.sort((a, b) => {
       return a.opciones.length - b.opciones.length;
     });
+
+    const celdasPorColapsar = celdasDisponibles.filter((celda) => {
+      return celda.opciones.length == celdasDisponibles[0].opciones.length;
+    });
+
+    const celdaSeleccionada = random(celdasPorColapsar);
+    celdaSeleccionada.colapsada = true;
+
+    const opcionSeleccionada = random(celdaSeleccionada.opciones);
+    celdaSeleccionada.opciones = [opcionSeleccionada];
+
+    //print(celdaSeleccionada);
+
+    for (let x = 0; x < RETICULA; x++) {
+      for (let y = 0; y < RETICULA; y++) {
+        const celdaIndex = x + y * RETICULA;
+        const celdaActual = celdas[celdaIndex];
+        if (celdaActual.colapsada) {
+          const indiceDeAzulejo = celdaActual.opciones[0];
+          const reglasActuales = reglas[indiceDeAzulejo];
+          //print(reglasActuales);
+          image(azulejos[indiceDeAzulejo], x * ancho, y * alto, ancho, alto);
+
+          //Cambiar entropía UP
+          if (y > 0) {
+            const indiceUP = x + (y - 1) * RETICULA;
+            const celdaUp = celdas[indiceUP];
+            if (!celdaUp.colapsada) {
+              cambiarEntropia(celdaUp, reglasActuales[`UP`], `DOWN`);
+            }
+          }
+
+          // Cambiar entropía RIGHT
+          if (x < RETICULA - 1) {
+            const indiceRight = x + 1 + y * RETICULA;
+            const celdaRight = celdas[indiceRight];
+            if (!celdaRight.colapsada) {
+              cambiarEntropia(celdaRight, reglasActuales[`RIGHT`], `LEFT`);
+            }
+          }
+
+          // Cambiar entropía DOWN
+          if (y < RETICULA - 1) {
+            const indiceDown = x + (y + 1) * RETICULA;
+            const celdaDown = celdas[indiceDown];
+            if (!celdaDown.colapsada) {
+              cambiarEntropia(celdaDown, reglasActuales[`DOWN`], `UP`);
+            }
+          }
+
+          // Cambiar entropía LEFT
+          if (x > 0) {
+            const indiceLeft = x - 1 + y * RETICULA;
+            const celdaLeft = celdas[indiceLeft];
+            if (!celdaLeft.colapsada) {
+              cambiarEntropia(celdaLeft, reglasActuales["LEFT"], "RIGHT");
+            }
+          }
+        } else {
+          //strokeWeight(6);
+          //rect(x * ancho, y * alto, ancho, alto);
+        }
+      }
+      //noLoop();
+    }
   }
-  print(celdasDisponibles);
-  noLoop();
+}
+
+function cambiarEntropia(_celda, _regla, _opuesta) {
+  const nuevasOpciones = [];
+  for (let i = 0; i < _celda.opciones.length; i++) {
+    if (_regla == reglas[_celda.opciones[i]][_opuesta]) {
+      const celdaCompatible = _celda.opciones[i];
+      nuevasOpciones.push(celdaCompatible);
+    }
+  }
+  _celda.opciones = nuevasOpciones;
+  print(nuevasOpciones);
 }
